d123d6bb87a1550943471c5387e0cf7bb582c9ff
moog <moogen@aistmail.com>
2019-09-05 18:02:20 +0600

    Исправил поиск пакетов

--- Sync.cs
+++ Sync.cs
@@ -1,17 +1,11 @@
 ﻿using Inedo.UPack;
 using Inedo.UPack.Net;
-using Newtonsoft.Json.Linq;
-using RestSharp;
-using RestSharp.Authenticators;
 using Serilog;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Security;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace updater
@@ -53,13 +47,11 @@ namespace updater
             var packages = await SourceFeed.ListPackagesAsync("", null);
             foreach (var p in packages)
             {
-                _log.Information($"Целевой пакет {p.Name} {p.LatestVersion}");
-
+                _log.Information("Целевой пакет {group}/{name}", p.Group, p.Name);
                 var search = await DestFeed.SearchPackagesAsync(p.Name);
-                //var search = await DestFeed.SearchPackagesAsync("master/pokermaster");
-                if (search.Count == 0)
+                if (!search.Any(x=>x.FullName == p.FullName))
                 {
-                    _log.Information($"Не нашел пакет {p.Group}/{p.Name} в {_programConfig.DestProGetUrl}/{_programConfig.DestProGetFeedName}, копирую из {_programConfig.SourceProGetUrl}/{_programConfig.SourceProGetFeedName}");
+                    _log.Information("Не нашел пакет {group}/{name} в {progetur}/{feedname}, копирую из {progeturl}/{progetfeed}", p.Group, p.Name, _programConfig.DestProGetUrl, _programConfig.DestProGetFeedName, _programConfig.SourceProGetUrl, _programConfig.SourceProGetFeedName);
 
 
                     foreach (var ver in p.AllVersions)
@@ -72,44 +64,42 @@ namespace updater
                         }
                         using (var fileStream = File.OpenRead($@"C:\temp\{p.Group}_{p.Name}_{ver}.upack"))
                         {
-                            _log.Information($"Копирую пакет {p.FullName} {ver} в {_programConfig.DestProGetUrl}");
+                            _log.Information("Копирую пакет {group}/{name} {version} в {progeturl}", p.Group, p.Name, ver, _programConfig.DestProGetUrl);
                             await DestFeed.UploadPackageAsync(fileStream);
                         }
-                        
+
                     }
 
                 }
 
-                _log.Information("Нашел пакет {0}, в локальном прогете, проверяю версии", p.Name);
                 foreach (var pack in search)
                 {
-                    if (p.Group == pack.Group)
-                        if (p.LatestVersion != pack.LatestVersion)
+                    if (p.FullName == pack.FullName)
+                    {
+                        _log.Information("Нашел пакет {group}/{name}, в локальном прогете, проверяю версии", p.Group, p.Name);
+                        foreach (var ver in p.AllVersions)
                         {
-                            foreach (var ver in p.AllVersions)
+                            if (!pack.AllVersions.Contains(ver))
                             {
-                                //bool s = Array.Exists(pack.AllVersions, vers => vers == ver);
-                                //if (Array.Exists(pack.AllVersions, v => v == ver.ToString()))
-                                if (!pack.AllVersions.Contains(ver))
+                                using (var packageStream = await SourceFeed.GetPackageStreamAsync(p.FullName, ver))
+                                using (var fileStream = File.Create($@"C:\temp\{p.Group}_{p.Name}_{ver}.upack"))
                                 {
-                                    using (var packageStream = await SourceFeed.GetPackageStreamAsync(p.FullName, ver))
-                                    using (var fileStream = File.Create($@"C:\temp\{p.Group}_{p.Name}_{ver}.upack"))
-                                    {
-                                        await packageStream.CopyToAsync(fileStream);
+                                    await packageStream.CopyToAsync(fileStream);
 
-                                    }
-                                    using (var fileStream = File.OpenRead($@"C:\temp\{p.Group}_{p.Name}_{ver}.upack"))
-                                    {
-                                        _log.Information($"Копирую пакет {p.FullName} в {_programConfig.DestProGetUrl}");
-                                        await DestFeed.UploadPackageAsync(fileStream);
-                                    }
                                 }
+                                using (var fileStream = File.OpenRead($@"C:\temp\{p.Group}_{p.Name}_{ver}.upack"))
+                                {
+                                    _log.Information("Копирую пакет {group}/{package} в {progeturl}", p.Group, p.Name, _programConfig.DestProGetUrl);
+                                    await DestFeed.UploadPackageAsync(fileStream);
+                                }
+
                             }
                         }
+                        _log.Information("Нет новых версий {group}/{name} в {progeturl}/{geedname}", p.Group, p.Name, _programConfig.SourceProGetUrl, _programConfig.SourceProGetFeedName);
 
-
+                    }
                 }
-                _log.Information("Не нашел свежих версий в {0}", _programConfig.SourceProGetUrl);
+
             }
 
         }
