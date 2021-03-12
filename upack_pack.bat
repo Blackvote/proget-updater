0c186a42f62a568feff3e69045da42d98152cf8d
Сергей Umka <umka@deeplay.io>
2021-03-12 11:47:05 +0600

    Add: Добавил upack_pack.bat + upack.json для упрощения сборки пакета

---
+++ upack_pack.bat
@@ -0,0 +1,5 @@
+@echo off
+:: Require: upack.exe in PATH from https://github.com/Inedo/upack/releases
+for /f "tokens=3delims=<>" %%a in ('findstr "Version" "updater.csproj"') do set csproj-version=%%a
+echo In updater.csproj: Version = "%csproj-version%"
+upack.exe pack ./bin/Release/netcoreapp2.2/publish --targetDirectory=./bin/Release/ --manifest=./upack.json
