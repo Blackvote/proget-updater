6741a85c6c2845c10deee5b3894967186122844b
Сергей Umka <umka@deeplay.io>
2021-04-27 18:47:11 +0600

    Исправил профили публикации под .NET Core 3.1 и проверил под win-x64 и linux-x64. Удалил старые профили под .NET Core 2.2.

--- upack_pack.bat
+++ upack_pack.bat
@@ -3,11 +3,5 @@
 for /f "tokens=3delims=<>" %%a in ('findstr "Version" "updater.csproj"') do set csproj-version=%%a
 echo In updater.csproj: Version = "%csproj-version%"
 
-echo Prepare release dir: .\bin\Release\upack
-del /F /S /Q .\bin\Release\upack
-mkdir .\bin\Release\upack
-copy /Y .\updater2\bin\Release\netcoreapp3.1\publish.full\* .\bin\Release\upack\
-copy /Y .\bin\Release\netcoreapp3.1\publish.full\* .\bin\Release\upack\
-
 echo Create upack-package
-upack.exe pack ./bin/Release/upack --targetDirectory=./bin/Release/ --manifest=./upack.json
+upack.exe pack ./bin/Release/netcoreapp3.1/publish/ --targetDirectory=./bin/Release/ --manifest=./upack.json
