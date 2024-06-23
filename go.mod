b19d9c09cfc8ce9cd6166dadc92de10ec1d6bbb7
Mateo <Mateo@deeplay.io>
2024-06-23 21:54:14 +0500

    ver 1.

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module goUpdater
 go 1.21
 
 require (
-	golang.org/x/sys v0.21.0
+	github.com/rs/zerolog v1.33.0
 	gopkg.in/yaml.v2 v2.4.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/sys v0.21.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,59 +5,170 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/rs/zerolog/log"
 	"gopkg.in/yaml.v2"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"strings"
 	"sync"
+	"syscall"
 	"time"
 )
 
 type Config struct {
-	Source      ProgetConfig `yaml:"source"`
-	Destination ProgetConfig `yaml:"destination"`
+	Source              ProgetConfig  `yaml:"source"`
+	Destination         ProgetConfig  `yaml:"destination"`
+	Timeout             TimeoutConfig `yaml:"timeout"`
+	ProceedPackageLimit int           `yaml:"proceedPackageLimit"`
 }
 
 type ProgetConfig struct {
 	URL    string `yaml:"url"`
-	APIKey string `yaml:"api_key"`
+	APIKey string `yaml:"apiKey"`
 	Feed   string `yaml:"feed"`
 }
 
 type Package struct {
-	Group    string   `json:"group"`
-	Name     string   `json:"name"`
-	Versions []string `json:"versions"`
+	Group    string   `yaml:"group"`
+	Name     string   `yaml:"name"`
+	Versions []string `yaml:"versions"`
+}
+
+type TimeoutConfig struct {
+	WebRequestTimeout int `yaml:"webRequestTimeout"`
+	IterationTimeout  int `yaml:"iterationTimeout"`
+}
+
+var (
+	configFile  *string = new(string)
+	savePath    *string = new(string)
+	logFilePath *string = new(string)
+	m           sync.Mutex
+)
+
+const maxRetries = 5
+
+func init() {
+	flag.StringVar(configFile, "c", "config.yml", "path to config file")
+	flag.StringVar(savePath, "p", "./packages", "path to save downloaded packages")
+	flag.StringVar(logFilePath, "l", "", "path to logfile")
+	flag.Parse()
+}
+
+func setupLogging(logFilePath string) (*os.File, error) {
+	logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	multiWriter := io.MultiWriter(os.Stdout, logFile)
+	log.Logger = log.Output(multiWriter).With().
+		Str("app", "Updater").
+		Logger()
+	return logFile, nil
 }
 
 func main() {
-	configFile := flag.String("f", "config.yml", "path to config file")
-	savePath := flag.String("p", "./packages", "path to save downloaded packages")
-	flag.Parse()
+	if *logFilePath != "" {
+		logFile, err := setupLogging(*logFilePath)
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to open log file")
+			return
+		}
+		defer logFile.Close()
+	}
+
+	log.Info().Msgf("Clean %s", *savePath)
+	err := deleteDirectoryContents(*savePath)
+	if err != nil {
+		log.Error().Err(err).Msg("Error deleting directory contents")
+	} else {
+		log.Info().Msg("Directory contents deleted successfully")
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-stop
+		log.Fatal().Msg("Get SIGTERM.")
+		cancel()
+	}()
+
+	run(ctx)
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Info().Msg("App stopped, ending of loop.")
+			return
+		case <-time.After(3 * time.Second):
+			go func() {
+				log.Info().Msg("Try to run")
+				run(ctx)
+			}()
+		}
+	}
+}
+
+func run(parentCtx context.Context) {
+	m.Lock()
+	defer m.Unlock()
+
+	ctx, cancel := context.WithTimeout(parentCtx, 30*time.Minute)
+	defer cancel()
+
+	log.Info().Time("timestamp", time.Now()).Msg("Application start")
 
 	config, err := readConfig(*configFile)
 	if err != nil {
-		log.Fatalf("Failed to read config: %v", err)
+		log.Fatal().Err(err).Msg("Failed to read config")
 	}
 
-	sourcePackages, err := getPackages(config.Source)
+	sourcePackages, err := getPackages(ctx, config.Source, config.Timeout)
 	if err != nil {
-		log.Fatalf("Failed to get packages from source: %v", err)
+		log.Fatal().Err(err).Msg("Failed to get packages from source")
 	}
 
-	destPackages, err := getPackages(config.Destination)
+	destPackages, err := getPackages(ctx, config.Destination, config.Timeout)
 	if err != nil {
-		log.Fatalf("Failed to get packages from destination: %v", err)
+		log.Fatal().Err(err).Msg("Failed to get packages from destination")
 	}
 
-	err = syncPackages(config, sourcePackages, destPackages, *savePath)
+	err = syncPackages(ctx, config, sourcePackages, destPackages, *savePath)
 	if err != nil {
-		log.Fatalf("Failed to sync packages: %v", err)
+		log.Fatal().Err(err).Msg("Failed to sync packages")
 	}
+	log.Info().Msgf("Pause %d second", config.Timeout.IterationTimeout)
+	time.Sleep(time.Duration(config.Timeout.IterationTimeout) * time.Second)
+}
+
+func deleteDirectoryContents(dir string) error {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		filePath := filepath.Join(dir, file.Name())
+		if file.IsDir() {
+			err = os.RemoveAll(filePath)
+		} else {
+			err = os.Remove(filePath)
+		}
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func readConfig(configFile string) (*Config, error) {
@@ -75,43 +186,53 @@ func readConfig(configFile string) (*Config, error) {
 	return &config, nil
 }
 
-func getPackages(progetConfig ProgetConfig) ([]Package, error) {
+func getPackages(ctx context.Context, progetConfig ProgetConfig, timeoutConfig TimeoutConfig) ([]Package, error) {
 	url := fmt.Sprintf("%s/upack/%s/packages", progetConfig.URL, progetConfig.Feed)
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
 
 	req.Header.Set("X-ApiKey", progetConfig.APIKey)
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("failed to get packages: %s", resp.Status)
+	client := &http.Client{
+		Timeout: time.Duration(timeoutConfig.IterationTimeout) * time.Second,
 	}
 
+	var resp *http.Response
 	var packages []Package
-	err = json.NewDecoder(resp.Body).Decode(&packages)
-	if err != nil {
-		return nil, err
+
+	for attempt := 1; attempt <= maxRetries; attempt++ {
+		log.Info().Str("url", url).Msgf("Attempt %d to get package list", attempt)
+		resp, err = client.Do(req)
+		if err == nil && resp.StatusCode == http.StatusOK {
+			defer resp.Body.Close()
+			err = json.NewDecoder(resp.Body).Decode(&packages)
+			if err != nil {
+				return nil, err
+			}
+
+			log.Info().Str("url", url).Msgf("Package count in %s/%s: %d", progetConfig.URL, progetConfig.Feed, len(packages))
+
+			var packageList strings.Builder
+			for _, pkg := range packages {
+				packageList.WriteString(fmt.Sprintf("%s/%s: %s | ", pkg.Group, pkg.Name, strings.Join(pkg.Versions, " ")))
+			}
+			log.Info().Str("url", url).Msg(packageList.String())
+
+			if err != nil {
+				log.Error().Str("url", url).Msgf("Failed to get package attempt %d, Error: %s", attempt, err)
+			}
+			return packages, nil
+		}
+		if resp != nil && err == nil {
+			log.Error().Str("url", url).Msgf("Failed to get package attempt %d, Status: %s", attempt, resp.Status)
+			resp.Body.Close()
+		}
+		return packages, nil
 	}
-
-	fmt.Printf("Количество пакетов, найденных в %s/%s: %d\n", progetConfig.URL, progetConfig.Feed, len(packages))
-
-	fmt.Printf("Список пакетов: ")
-	for _, pkg := range packages {
-		fmt.Printf("%s/%s: %s | ", pkg.Group, pkg.Name, strings.Join(pkg.Versions, " "))
-	}
-	fmt.Println()
-
-	return packages, nil
+	return nil, err
 }
-
-func syncPackages(config *Config, sourcePackages, destPackages []Package, savePath string) error {
+func syncPackages(ctx context.Context, config *Config, sourcePackages, destPackages []Package, savePath string) error {
 	destPackageMap := make(map[string]map[string]bool)
 	for _, pkg := range destPackages {
 		key := fmt.Sprintf("%s:%s", pkg.Group, pkg.Name)
@@ -133,22 +254,30 @@ func syncPackages(config *Config, sourcePackages, destPackages []Package, savePa
 		}
 	}
 
+	fmt.Println(len(sourcePackageMap))
+
+	if len(sourcePackageMap) < config.ProceedPackageLimit {
+		config.ProceedPackageLimit = len(sourcePackageMap)
+	}
+	semaphore := make(chan struct{}, config.ProceedPackageLimit)
 	var wg sync.WaitGroup
 	for _, pkg := range sourcePackages {
 		for _, version := range pkg.Versions {
 			key := fmt.Sprintf("%s:%s", pkg.Group, pkg.Name)
 			if !destPackageMap[key][version] {
-				fmt.Printf("S-D %s:%s:%s not found. Syncing\n", pkg.Group, pkg.Name, version)
+				log.Info().Str("url", config.Destination.URL).Msgf("%s:%s:%s not found. Syncing", pkg.Group, pkg.Name, version)
 				wg.Add(1)
 				go func(pkg Package, version string) {
 					defer wg.Done()
-					err := downloadAndUploadPackage(config, pkg, version, savePath)
+					semaphore <- struct{}{}
+					defer func() { <-semaphore }()
+					err := downloadAndUploadPackage(ctx, config, pkg, version, savePath)
 					if err != nil {
-						log.Printf("Failed to sync package %s:%s: %v", pkg.Name, version, err)
+						log.Error().Err(err).Str("url", config.Destination.URL).Msgf("Failed to sync package %s:%s", pkg.Name, version)
 					}
 				}(pkg, version)
 			} else {
-				fmt.Printf("S-D %s:%s:%s found.\n", pkg.Group, pkg.Name, version)
+				log.Info().Str("url", config.Destination.URL).Msgf("%s:%s:%s found.", pkg.Group, pkg.Name, version)
 			}
 		}
 	}
@@ -160,13 +289,15 @@ func syncPackages(config *Config, sourcePackages, destPackages []Package, savePa
 				wg.Add(1)
 				go func(pkg Package, version string) {
 					defer wg.Done()
-					err := deleteFile(config, pkg, version)
+					semaphore <- struct{}{}
+					defer func() { <-semaphore }()
+					err := deleteFile(ctx, config, pkg, version)
 					if err != nil {
-						log.Printf("Failed to sync package %s:%s: %v\n", pkg.Name, version, err)
+						log.Error().Err(err).Str("url", config.Destination.URL).Msgf("Failed to delete package %s:%s", pkg.Name, version)
 					}
 				}(pkg, version)
 			} else {
-				fmt.Printf("D-S %s:%s:%s found.\n", pkg.Group, pkg.Name, version)
+				log.Info().Str("url", config.Destination.URL).Msgf("%s:%s:%s found.", pkg.Group, pkg.Name, version)
 			}
 		}
 	}
@@ -174,129 +305,147 @@ func syncPackages(config *Config, sourcePackages, destPackages []Package, savePa
 	return nil
 }
 
-func downloadAndUploadPackage(config *Config, pkg Package, version, savePath string) error {
+func downloadAndUploadPackage(ctx context.Context, config *Config, pkg Package, version, savePath string) error {
 	downloadURL := fmt.Sprintf("%s/upack/%s/download/%s/%s/%s", config.Source.URL, config.Source.Feed, pkg.Group, pkg.Name, version)
 	uploadURL := fmt.Sprintf("%s/upack/%s/upload", config.Destination.URL, config.Destination.Feed)
 
-	packagePath := filepath.Join(savePath)
+	err := os.MkdirAll(savePath, os.ModePerm)
+	if err != nil {
+		log.Error().Msgf("Failed to create dir %s", savePath)
+	}
 
-	os.MkdirAll(packagePath, os.ModePerm)
+	filePath := filepath.Join(savePath, fmt.Sprintf("%s.%s.upack", pkg.Name, version))
 
-	filePath := filepath.Join(packagePath, fmt.Sprintf("%s.%s.upack", pkg.Name, version))
-
-	err := downloadFile(downloadURL, config.Source.APIKey, filePath)
+	err = downloadFile(ctx, downloadURL, config.Source.APIKey, filePath, config.Timeout)
 	if err != nil {
 		return err
 	}
 
-	return uploadFile(uploadURL, config.Destination.APIKey, filePath)
+	return uploadFile(ctx, uploadURL, config.Destination.APIKey, filePath, config.Timeout)
 }
 
-func downloadFile(url, apiKey, filePath string) error {
-	fmt.Printf("Скачивание пакета %s\n", filePath)
-	//req, err := http.NewRequest("GET", url, nil)
-	//if err != nil {
-	//	return err
-	//}
-
-	// Создаем контекст с тайм-аутом
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
+func downloadFile(ctx context.Context, url, apiKey, filePath string, timeoutConfig TimeoutConfig) error {
+	log.Info().Str("url", url).Msgf("Download package %s", filePath)
 
 	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
 		return err
 	}
-
 	req.Header.Set("X-ApiKey", apiKey)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
+	client := &http.Client{
+		Timeout: time.Duration(timeoutConfig.WebRequestTimeout) * time.Second,
 	}
-	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return fmt.Errorf("cant read body from request. Failed to download file: %s", resp.Status)
+	for attempt := 1; attempt <= maxRetries; attempt++ {
+		log.Info().Str("url", url).Msgf("Attempt download %d", attempt)
+
+		resp, err := client.Do(req)
+		if err == nil && resp.StatusCode == http.StatusOK {
+			defer resp.Body.Close()
+
+			out, err := os.Create(filePath)
+			if err != nil {
+				return err
+			}
+			defer out.Close()
+
+			fileSize, err := io.Copy(out, resp.Body)
+			if err != nil {
+				log.Error().Err(err).Str("url", url).Msgf("Failed to copy response body")
+				return err
+			}
+
+			fileSizeMB := float64(fileSize) / (1024 * 1024)
+			log.Info().Str("url", url).Msgf("%s file Size: %.2f MB", strings.TrimSuffix(strings.TrimPrefix(filePath, "packages\\"), ".upack"), fileSizeMB)
+			log.Info().Str("url", url).Msgf("Success download from ")
+			return nil
 		}
-		return fmt.Errorf("failed to download file: %s (%s)", string(bodyBytes), resp.Status)
-	} else {
-		fmt.Printf("Скачан пакета %s\n", filePath)
+
+		// Закрываем тело ответа при ошибке или статусе не 200
+		if resp != nil {
+			log.Error().Str("url", url).Msgf("Failed download attempt %d, Status: %s", attempt, resp.Status)
+			resp.Body.Close()
+		}
+
+		if err != nil {
+			log.Error().Str("url", url).Msgf("Failed download attempt %d for file %s, Error: %s", attempt, strings.TrimSuffix(strings.TrimPrefix(filePath, "packages\\"), ".upack"), err)
+		}
+
+		time.Sleep(3 * time.Duration(attempt) * time.Second)
 	}
 
-	out, err := os.Create(filePath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	_, err = io.Copy(out, resp.Body)
-	return err
+	return fmt.Errorf("failed to retrieve %s after %d attempts", url, maxRetries)
 }
 
-func uploadFile(url, apiKey, filePath string) error {
-	fmt.Printf("Загрузка пакета %s\n", filePath)
+func uploadFile(ctx context.Context, url, apiKey, filePath string, timeoutConfig TimeoutConfig) error {
+	log.Info().Str("url", url).Msgf("Upload package %s", strings.TrimSuffix(strings.TrimPrefix(filePath, "packages\\"), ".upack"))
 	file, err := os.Open(filePath)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	req, err := http.NewRequest("POST", url, file)
+	req, err := http.NewRequestWithContext(ctx, "POST", url, file)
 	if err != nil {
 		return err
 	}
 	req.Header.Set("X-ApiKey", apiKey)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
+	client := &http.Client{
+		Timeout: time.Duration(timeoutConfig.WebRequestTimeout) * time.Second,
 	}
-	defer resp.Body.Close()
 
-	if resp.StatusCode != 201 {
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return fmt.Errorf("cant read body from request. Failed to upload file: %s", resp.Status)
+	for attempt := 1; attempt <= maxRetries; attempt++ {
+		log.Info().Str("url", url).Msgf("Attempt %d upload for file %s", attempt, strings.TrimSuffix(strings.TrimPrefix(filePath, "packages\\"), ".upack"))
+
+		resp, err := client.Do(req)
+		if err == nil && resp.StatusCode == http.StatusCreated {
+			defer resp.Body.Close()
+			log.Info().Str("url", url).Msgf("Success upload: for file %s", strings.TrimSuffix(strings.TrimPrefix(filePath, "packages\\"), ".upack"))
+			err = os.Remove(filePath)
+			return nil
 		}
-		return fmt.Errorf("failed to upload file: %s (%s)", string(bodyBytes), resp.Status)
-	} else {
-		fmt.Printf("Загружен пакет %s\n", filePath)
-	}
 
-	return nil
+		if resp != nil {
+			log.Error().Str("url", url).Msgf("Failed upload attempt %d for file %s, Status: %s", attempt, strings.TrimSuffix(strings.TrimPrefix(filePath, "packages\\"), ".upack"), resp.Status)
+			resp.Body.Close()
+		}
+
+		if err != nil {
+			log.Error().Str("url", url).Msgf("Failed upload attempt %d for file %s, Error: %s", attempt, strings.TrimSuffix(strings.TrimPrefix(filePath, "packages\\"), ".upack"), err)
+		}
+
+		time.Sleep(3 * time.Duration(attempt) * time.Second)
+	}
+	return err
 }
 
-func deleteFile(config *Config, pkg Package, version string) error {
-	fmt.Printf("Пакет: %s/%s помечен для удаления из %s\n", pkg.Group, pkg.Name, config.Destination.URL)
-
+func deleteFile(ctx context.Context, config *Config, pkg Package, version string) error {
 	deleteURL := fmt.Sprintf("%s/upack/%s/delete/%s/%s/%s", config.Destination.URL, config.Destination.Feed, pkg.Group, pkg.Name, version)
-	req, err := http.NewRequest("POST", deleteURL, nil)
+	log.Info().Str("url", deleteURL).Msgf("Package: %s/%s mark for delete", pkg.Group, pkg.Name)
+
+	req, err := http.NewRequestWithContext(ctx, "POST", deleteURL, nil)
 	if err != nil {
 		return err
 	}
+
 	req.Header.Set("X-ApiKey", config.Destination.APIKey)
-
 	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return fmt.Errorf("cant read body from request. Failed to delete file: %s", resp.Status)
+	for attempt := 1; attempt <= maxRetries; attempt++ {
+		log.Info().Str("url", deleteURL).Msgf("Attempt %d to delete %s:%s", attempt, pkg.Group, pkg.Name)
+
+		resp, err := client.Do(req)
+		if err == nil && resp.StatusCode == http.StatusOK {
+			defer resp.Body.Close()
+			log.Info().Str("url", deleteURL).Msgf("Success delete %s:%s", pkg.Group, pkg.Name)
+			return nil
 		}
-		return fmt.Errorf("failed to delete file: %s (%s)", string(bodyBytes), resp.Status)
-	} else {
-		fmt.Printf("Пакет: %s/%s удалён из %s\n", pkg.Group, pkg.Name, config.Destination.URL)
-	}
 
+		if resp != nil {
+			log.Error().Str("url", deleteURL).Msgf("Attempt %d failed to delete %s:%s. Status: %s", attempt, pkg.Group, pkg.Name, resp.Status)
+			resp.Body.Close()
+		}
+		time.Sleep(3 * time.Duration(attempt) * time.Second)
+	}
 	return err
 }
